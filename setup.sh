d1e8e7a23d019dd315758e91fc45d3e22cdf34e7
Kimitoshi Takahashi <ktaka@ccmp.jp>
2020-01-23 02:34:55 +0900

    wip

--- setup.sh
+++ setup.sh
@@ -3,8 +3,11 @@
 sudo aptitude install python3-pip python3-dev -y
 sudo aptitude install gcc libffi-dev libssl-dev git -y
 
-sudo pip install --upgrade pip
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 2
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 3
 
-sudo pip install markupsafe ansible
-sudo pip install cryptography
+#sudo pip3 install --upgrade pip3
+
+sudo pip3 install markupsafe ansible
+sudo pip3 install cryptography
 
